5c10454bbe347ca172bbf7e9baf10dd527a1b933
Patryk Kowalski <patryk.kowalski@deepsense.ai>
2023-07-18 19:37:21 +0200

    Implemented diff, language_composition functions. Got a local review between branches into file working.

--- pr_agent/git_providers/local_git_provider.py
+++ pr_agent/git_providers/local_git_provider.py
@@ -1,11 +1,11 @@
 import logging
+import os
 from typing import List
 
 from git import Repo
-from github.PullRequest import PullRequest
 
 from pr_agent.config_loader import settings
-from pr_agent.git_providers.git_provider import GitProvider, FilePatchInfo
+from pr_agent.git_providers.git_provider import GitProvider, FilePatchInfo, EDIT_TYPE
 
 
 class PullRequestMimic():
@@ -14,8 +14,9 @@ class PullRequestMimic():
     It only implements methods used by the GitHubProvider class.
     '''
 
-    def __init__(self, title):
+    def __init__(self, title: str, diff_files: List[FilePatchInfo]):
         self.title = title
+        self.diff_files = diff_files
 
 
 class LocalGitProvider(GitProvider):
@@ -30,15 +31,40 @@ class LocalGitProvider(GitProvider):
         self.repo = Repo(settings.get("local.path"))
         self.branch_name = branch_name
         self.diff_files = None
-        self.pr = PullRequestMimic(self.get_pr_title())
+        self.pr = PullRequestMimic(self.get_pr_title(), self.get_diff_files())
+        self.output_file_path = settings.get("local.output")
+        # TODO Implement inline code comments for local git provider
+        settings.pr_reviewer.inline_code_comments = False
 
     def is_supported(self, capability: str) -> bool:
         # TODO implement
         pass
 
     def get_diff_files(self) -> list[FilePatchInfo]:
-        # TODO implement
-        pass
+        diffs = self.repo.head.commit.diff(self.repo.branches[self.branch_name].commit, create_patch=True)
+        diff_files = []
+        for diff_item in diffs:
+            if diff_item.a_blob is not None:
+                original_file_content_str = diff_item.a_blob.data_stream.read().decode('utf-8')
+            else:
+                original_file_content_str = ""  # empty file
+            if diff_item.b_blob is not None:
+                new_file_content_str = diff_item.b_blob.data_stream.read().decode('utf-8')
+            else:
+                new_file_content_str = ""  # empty file
+            edit_type = EDIT_TYPE.MODIFIED
+            if diff_item.new_file:
+                edit_type = EDIT_TYPE.ADDED
+            elif diff_item.deleted_file:
+                edit_type = EDIT_TYPE.DELETED
+            elif diff_item.renamed_file:
+                edit_type = EDIT_TYPE.RENAMED
+            diff_files.append(
+                FilePatchInfo(original_file_content_str, new_file_content_str, diff_item.diff, diff_item.a_path,
+                              edit_type=edit_type,
+                              old_filename=None if diff_item.b_path == diff_item.a_path else diff_item.b_path))
+        self.diff_files = diff_files
+        return diff_files
 
     def get_files(self) -> List[str]:
         '''
@@ -60,7 +86,9 @@ class LocalGitProvider(GitProvider):
         raise NotImplementedError('Publishing descriptions is not implemented for the local git provider')
 
     def publish_comment(self, pr_comment: str, is_temporary: bool = False):
-        logging.info(pr_comment)
+        with open(self.output_file_path, "w") as file:
+            # Write the string to the file
+            file.write(pr_comment)
 
     def publish_inline_comment(self, body: str, relevant_file: str, relevant_line_in_file: str):
         raise NotImplementedError('Publishing inline comments is not implemented for the local git provider')
@@ -76,16 +104,28 @@ class LocalGitProvider(GitProvider):
         raise NotImplementedError('Publishing code suggestions is not implemented for the local git provider')
 
     def remove_initial_comment(self):
-        raise NotImplementedError('Removing initial comments is not implemented for the local git provider')
+        pass # Not applicable to local git provider, but required by the interface
 
     def get_languages(self):
-        pass
+        # Get all files in repository
+        files = [item.path for item in self.repo.tree().traverse() if item.type == 'blob']
+        # Identify language by file extension and count
+        lang_count = {}
+        total_files = 0
+        for filepath in files:
+            ext = os.path.splitext(filepath)[1]
+            lang = ext.lstrip('.').lower()  # Remove the dot and convert to lowercase
+            lang_count[lang] = lang_count.get(lang, 0) + 1
+            total_files += 1
+        # Convert counts to percentages
+        lang_percentage = {lang: count / total_files * 100 for lang, count in lang_count.items()}
+        return lang_percentage
 
     def get_pr_branch(self):
         return self.repo.head
 
     def get_user_id(self):
-        raise NotImplementedError('Getting user id is not implemented for the local git provider')
+        return -1 # Not used anywhere for local provider, but required by the interface
 
     def get_pr_description(self):
         commits_diff = list(self.repo.iter_commits(self.branch_name + '..HEAD'))
