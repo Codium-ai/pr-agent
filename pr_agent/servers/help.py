67c61e0ac8c5b163ae2f71d75f8f37cb45a620b7
Ori Kotek <ori.k@codium.ai>
2023-07-19 17:27:12 +0300

    Update help

--- pr_agent/servers/help.py
+++ pr_agent/servers/help.py
@@ -1,9 +1,9 @@
-commands_text = "> /review      Request a review of the latest update to the PR.\n" \
-                "> /review -i   Incremental review - consider only the changes from the last review.\n" \
-                "> /describe    Modify the PR title and description based on the contents of the PR.\n" \
-                "> /improve     Suggest improvements to the code in the PR. " \
+commands_text = "> /review [-i]: Request a review of your Pull Request. For an incremental review, which only " \
+                "considers changes since the last review, include the '-i' option.\n" \
+                "> /describe: Modify the PR title and description based on the contents of the PR.\n" \
+                "> /improve: Suggest improvements to the code in the PR. " \
                 "These will be provided as pull request comments, ready to commit.\n" \
-                "> /ask <QUESTION>  Pose a question about the PR.\n"
+                "> /ask <QUESTION>: Pose a question about the PR.\n"
 
 
 def bot_help_text(user: str):
