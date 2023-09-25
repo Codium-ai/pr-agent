8f81c18647536404039ed2c086bba0a5b0a619b7
mrT23 <tal.r@codium.ai>
2023-09-25 18:01:32 +0300

    auto commands in github action

--- pr_agent/servers/github_action_runner.py
+++ pr_agent/servers/github_action_runner.py
@@ -5,6 +5,8 @@ import os
 from pr_agent.agent.pr_agent import PRAgent
 from pr_agent.config_loader import get_settings
 from pr_agent.git_providers import get_git_provider
+from pr_agent.tools.pr_code_suggestions import PRCodeSuggestions
+from pr_agent.tools.pr_description import PRDescription
 from pr_agent.tools.pr_reviewer import PRReviewer
 
 
@@ -53,7 +55,15 @@ async def run_action():
         if action in ["opened", "reopened"]:
             pr_url = event_payload.get("pull_request", {}).get("url")
             if pr_url:
-                await PRReviewer(pr_url).run()
+                auto_review = os.environ.get('github_action.auto_review')
+                if auto_review is None or auto_review.lower() == 'true':
+                    await PRReviewer(pr_url).run()
+                auto_describe = os.environ.get('github_action.auto_describe')
+                if auto_describe.lower() == 'true':
+                    await PRDescription(pr_url).run()
+                auto_improve = os.environ.get('github_action.auto_improve')
+                if auto_improve.lower() == 'true':
+                    await PRCodeSuggestions(pr_url).run()
 
     # Handle issue comment event
     elif GITHUB_EVENT_NAME == "issue_comment":
