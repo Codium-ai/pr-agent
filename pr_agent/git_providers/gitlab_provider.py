4bda9dfe04cf33f11e4c763fd14cb510e62d6e85
Ori Kotek <ori.k@codium.ai>
2023-10-30 17:01:49 +0200

    Update get_repo_settings to decode file from target branch in gitlab_provider.py

--- pr_agent/git_providers/gitlab_provider.py
+++ pr_agent/git_providers/gitlab_provider.py
@@ -315,7 +315,7 @@ class GitLabProvider(GitProvider):
 
     def get_repo_settings(self):
         try:
-            contents = self.gl.projects.get(self.id_project).files.get(file_path='.pr_agent.toml', ref=self.mr.source_branch).decode()
+            contents = self.gl.projects.get(self.id_project).files.get(file_path='.pr_agent.toml', ref=self.mr.target_branch).decode()
             return contents
         except Exception:
             return ""
