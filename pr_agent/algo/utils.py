54f41dd603180cab03e674e31e17c31ec63e1c88
mrT23 <tal.r@codium.ai>
2023-11-07 14:41:15 +0200

    code

--- pr_agent/algo/utils.py
+++ pr_agent/algo/utils.py
@@ -348,7 +348,7 @@ def get_user_labels(current_labels):
 def get_max_tokens(model):
     settings = get_settings()
     max_tokens_model = MAX_TOKENS[model]
-    if settings.config.max_model_tokens and max_tokens_model > settings.config.max_model_tokens:
-        max_tokens_model = settings.config.max_model_tokens
+    if settings.config.max_model_tokens:
+        max_tokens_model = min(settings.config.max_model_tokens, max_tokens_model)
         # get_logger().debug(f"limiting max tokens to {max_tokens_model}")
     return max_tokens_model
