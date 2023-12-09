b6409929d242b17ed695a71cf045b7f03f4dd1dc
Brian Pham <6511700+brianpham93@users.noreply.github.com>
2023-12-09 16:51:50 +0000

    Remove extra code

--- pr_agent/algo/base_ai_handler.py
+++ pr_agent/algo/base_ai_handler.py
@@ -18,31 +18,3 @@ class BaseAiHandler(ABC):
     async def chat_completion(self, model: str, system: str, user: str, temperature: float = 0.2):
         pass
 
-
-class AiHandler(BaseAiHandler):
-    """
-    This class handles interactions with the OpenAI API for chat completions.
-    It initializes the API key and other settings from a configuration file,
-    and provides a method for performing chat completions using the OpenAI ChatCompletion API.
-    """
-
-    # ... rest of your code ...
-
-
-class CustomAiHandler(BaseAiHandler):
-    """
-    This class is your custom AI handler that uses a different LLM library.
-    """
-
-    def __init__(self):
-        # Initialize your custom AI handler
-        pass
-
-    @property
-    def deployment_id(self):
-        # Return the deployment ID for your custom AI handler
-        pass
-
-    async def chat_completion(self, model: str, system: str, user: str, temperature: float = 0.2):
-        # Implement the chat completion method for your custom AI handler
-        pass
