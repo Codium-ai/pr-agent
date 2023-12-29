a15673242f0a74546f685f45367d1b2695d0039a
suixinran <suixinran@lucidata.cn>
2023-12-29 10:47:43 +0800

    归并

--- pr_agent/algo/utils.py
+++ pr_agent/algo/utils.py
@@ -475,3 +475,36 @@ def clip_tokens(text: str, max_tokens: int, add_three_dots=True) -> str:
     except Exception as e:
         get_logger().warning(f"Failed to clip tokens: {e}")
         return text
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        # 合并两个有序数组
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # 处理剩余的元素
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
