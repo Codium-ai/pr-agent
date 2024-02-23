2f3171e42237d3d035fb0e9b585b51d7f8fb11df
Ori Kotek <ori.k@codium.ai>
2024-02-23 18:27:34 +0200

    Change github app startup logic to support gunicorj

--- pr_agent/servers/github_app.py
+++ pr_agent/servers/github_app.py
@@ -280,17 +280,15 @@ async def root():
     return {"status": "ok"}
 
 
-def start():
-    if get_settings().github_app.override_deployment_type:
-        # Override the deployment type to app
-        get_settings().set("GITHUB.DEPLOYMENT_TYPE", "app")
-    get_settings().set("CONFIG.PUBLISH_OUTPUT_PROGRESS", False)
-    middleware = [Middleware(RawContextMiddleware)]
-    app = FastAPI(middleware=middleware)
-    app.include_router(router)
+if get_settings().github_app.override_deployment_type:
+    # Override the deployment type to app
+    get_settings().set("GITHUB.DEPLOYMENT_TYPE", "app")
+get_settings().set("CONFIG.PUBLISH_OUTPUT_PROGRESS", False)
+middleware = [Middleware(RawContextMiddleware)]
+app = FastAPI(middleware=middleware)
+app.include_router(router)
 
+
+def start():
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", "3000")))
 
-
-if __name__ == '__main__':
-    start()
