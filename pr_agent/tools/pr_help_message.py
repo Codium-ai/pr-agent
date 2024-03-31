c3b3651769c8b42abf82dacf69cca9c988473c28
mrT23 <tal.r@codium.ai>
2024-03-31 11:43:00 +0300

    refine help

--- pr_agent/tools/pr_help_message.py
+++ pr_agent/tools/pr_help_message.py
@@ -27,12 +27,13 @@ class PRHelpMessage:
             tool_names.append(f"[DESCRIBE]({base_path}/describe/)")
             tool_names.append(f"[REVIEW]({base_path}/review/)")
             tool_names.append(f"[IMPROVE]({base_path}/improve/)")
-            tool_names.append(f"[ANALYZE]({base_path}/analyze/) 💎")
             tool_names.append(f"[UPDATE CHANGELOG]({base_path}/update_changelog/)")
-            tool_names.append(f"[ADD DOCUMENTATION]({base_path}/documentation/) 💎")
-            tool_names.append(f"[ASK]({base_path}/ask/)")
-            tool_names.append(f"[GENERATE CUSTOM LABELS]({base_path}/custom_labels/)")
+            tool_names.append(f"[ADD DOCS]({base_path}/documentation/) 💎")
             tool_names.append(f"[TEST]({base_path}/test/) 💎")
+            tool_names.append(f"[IMPROVE COMPONENT]({base_path}/improve_component/) 💎")
+            tool_names.append(f"[ANALYZE]({base_path}/analyze/) 💎")
+            tool_names.append(f"[ASK]({base_path}/ask/)")
+            tool_names.append(f"[GENERATE CUSTOM LABELS]({base_path}/custom_labels/) 💎")
             tool_names.append(f"[CI FEEDBACK]({base_path}/ci_feedback/) 💎")
             tool_names.append(f"[CUSTOM SUGGESTIONS]({base_path}/custom_suggestions/) 💎")
             tool_names.append(f"[SIMILAR ISSUE]({base_path}/similar_issues/)")
@@ -40,27 +41,29 @@ class PRHelpMessage:
             descriptions = []
             descriptions.append("Generates PR description - title, type, summary, code walkthrough and labels")
             descriptions.append("Adjustable feedback about the PR, possible issues, security concerns, review effort and more")
-            descriptions.append("Code suggestions for improving the PR.")
-            descriptions.append("Identifies code components that changed in the PR, and enables to interactively generate tests, docs, and code suggestions for each component.")
-            descriptions.append("Automatically updates the changelog.")
-            descriptions.append("Generates documentation to methods/functions/classes that changed in the PR.")
-            descriptions.append("Answering free-text questions about the PR.")
+            descriptions.append("Code suggestions for improving the PR")
+            descriptions.append("Automatically updates the changelog")
+            descriptions.append("Generates documentation to methods/functions/classes that changed in the PR")
+            descriptions.append("Generates unit tests for a specific component, based on the PR code change")
+            descriptions.append("Code suggestions for a specific component that changed in the PR")
+            descriptions.append("Identifies code components that changed in the PR, and enables to interactively generate tests, docs, and code suggestions for each component")
+            descriptions.append("Answering free-text questions about the PR")
             descriptions.append("Generates custom labels for the PR, based on specific guidelines defined by the user")
-            descriptions.append("Generates unit tests for a specific component, based on the PR code change.")
-            descriptions.append("Generates feedback and analysis for a failed CI job.")
-            descriptions.append("Generates custom suggestions for improving the PR code, based on specific guidelines defined by the user.")
-            descriptions.append("Automatically retrieves and presents similar issues.")
+            descriptions.append("Generates feedback and analysis for a failed CI job")
+            descriptions.append("Generates custom suggestions for improving the PR code, based only on specific guidelines defined by the user")
+            descriptions.append("Automatically retrieves and presents similar issues")
 
             commands  =[]
             commands.append("`/describe`")
             commands.append("`/review`")
             commands.append("`/improve`")
-            commands.append("`/analyze`")
             commands.append("`/update_changelog`")
             commands.append("`/add_docs`")
+            commands.append("`/test`")
+            commands.append("`/improve_component`")
+            commands.append("`/analyze`")
             commands.append("`/ask`")
             commands.append("`/generate_labels`")
-            commands.append("`/test`")
             commands.append("`/checks`")
             commands.append("`/custom_suggestions`")
             commands.append("`/similar_issue`")
@@ -69,9 +72,13 @@ class PRHelpMessage:
             checkbox_list.append(" - [ ] Run <!-- /describe -->")
             checkbox_list.append(" - [ ] Run <!-- /review -->")
             checkbox_list.append(" - [ ] Run <!-- /improve -->")
-            checkbox_list.append(" - [ ] Run <!-- /analyze -->")
             checkbox_list.append(" - [ ] Run <!-- /update_changelog -->")
             checkbox_list.append(" - [ ] Run <!-- /add_docs -->")
+            checkbox_list.append(" - [ ] Run <!-- /test -->")
+            checkbox_list.append(" - [ ] Run <!-- /improve_component -->")
+            checkbox_list.append(" - [ ] Run <!-- /analyze -->")
+            checkbox_list.append("[*]")
+            checkbox_list.append("[*]")
             checkbox_list.append("[*]")
             checkbox_list.append("[*]")
             checkbox_list.append("[*]")
@@ -80,16 +87,16 @@ class PRHelpMessage:
             checkbox_list.append("[*]")
 
             if isinstance(self.git_provider, GithubProvider):
-                pr_comment += f"<table><tr align='center'><th align='center'>Tool</th><th align='center'>Description</th><th align='center'>Invoke Interactively :gem:</th></tr>"
+                pr_comment += f"<table><tr align='left'><th align='left'>Tool</th><th align='left'>Description</th><th align='left'>Trigger Interactively :gem:</th></tr>"
                 for i in range(len(tool_names)):
-                    pr_comment += f"\n<tr><td align='center'>\n\n<strong>{tool_names[i]}</strong></td>\n<td>{descriptions[i]}</td>\n<td>\n\n{checkbox_list[i]}\n</td></tr>"
+                    pr_comment += f"\n<tr><td align='left'>\n\n<strong>{tool_names[i]}</strong></td>\n<td>{descriptions[i]}</td>\n<td>\n\n{checkbox_list[i]}\n</td></tr>"
                 pr_comment += "</table>\n\n"
                 pr_comment += f"""\n\n(1) Note that each tool be [triggered automatically](https://github.com/Codium-ai/pr-agent/blob/main/Usage.md#github-app-automatic-tools-for-pr-actions) when a new PR is opened, or called manually by [commenting on a PR](https://github.com/Codium-ai/pr-agent/blob/main/Usage.md#online-usage)."""
                 pr_comment += f"""\n\n(2) Tools marked with [*] require additional parameters to be passed. For example, to invoke the `/ask` tool, you need to comment on a PR: `/ask "<question content>"`. See the relevant documentation for each tool for more details."""
             else:
-                pr_comment += f"<table><tr align='center'><th align='center'>Tool</th><th align='left'>Command</th><th align='left'>Description</th></tr>"
+                pr_comment += f"<table><tr align='left'><th align='left'>Tool</th><th align='left'>Command</th><th align='left'>Description</th></tr>"
                 for i in range(len(tool_names)):
-                    pr_comment += f"\n<tr><td align='center'>\n\n<strong>{tool_names[i]}</strong></td><td>{commands[i]}</td><td>{descriptions[i]}</td></tr>"
+                    pr_comment += f"\n<tr><td align='left'>\n\n<strong>{tool_names[i]}</strong></td><td>{commands[i]}</td><td>{descriptions[i]}</td></tr>"
                 pr_comment += "</table>\n\n"
                 pr_comment += f"""\n\nNote that each tool be [invoked automatically](https://pr-agent-docs.codium.ai/usage-guide/automations_and_usage/) when a new PR is opened, or called manually by [commenting on a PR](https://pr-agent-docs.codium.ai/usage-guide/automations_and_usage/#online-usage)."""
             if get_settings().config.publish_output:
