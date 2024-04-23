2e344365896dce4e6c694456ca77ff91b5c19fa8
tacascer <trandangtrithanh2000@gmail.com>
2024-04-22 20:25:32 -0400

    chore: update GPT3.5 models

--- pr_agent/algo/__init__.py
+++ pr_agent/algo/__init__.py
@@ -1,8 +1,9 @@
 MAX_TOKENS = {
     'text-embedding-ada-002': 8000,
-    'gpt-3.5-turbo': 4000,
+    'gpt-3.5-turbo': 16000,
+    'gpt-3.5-turbo-0125': 16000,
     'gpt-3.5-turbo-0613': 4000,
-    'gpt-3.5-turbo-0301': 4000,
+    'gpt-3.5-turbo-1106': 16000,
     'gpt-3.5-turbo-16k': 16000,
     'gpt-3.5-turbo-16k-0613': 16000,
     'gpt-4': 8000,
