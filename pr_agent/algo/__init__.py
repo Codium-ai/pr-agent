a3d4d6d86f3423ec297d3405ebc34998ba5a2a98
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2024-06-21 09:30:52 +0100

    feat: claude 3.5 sonnet support

--- pr_agent/algo/__init__.py
+++ pr_agent/algo/__init__.py
@@ -32,6 +32,7 @@ MAX_TOKENS = {
     'anthropic.claude-v1': 100000,
     'anthropic.claude-v2': 100000,
     'anthropic/claude-3-opus-20240229': 100000,
+    'anthropic/claude-3-5-sonnet-20240620': 100000,
     'bedrock/anthropic.claude-instant-v1': 100000,
     'bedrock/anthropic.claude-v2': 100000,
     'bedrock/anthropic.claude-v2:1': 100000,
