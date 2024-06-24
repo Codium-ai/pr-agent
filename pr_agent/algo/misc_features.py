5255c5a5d9a491dd9e35eb119a0e24ff0c7ebede
Jie Zhang <jie.zhang@aofl.com>
2024-06-24 15:20:34 -0700

    Handle index error and better performance

--- pr_agent/algo/misc_features.py
+++ pr_agent/algo/misc_features.py
@@ -2,13 +2,15 @@ from slack_sdk import WebClient
 import re
 from pr_agent.config_loader import get_settings
 from pr_agent.git_providers.git_provider import GitProvider
+from pr_agent.log import get_logger
 
 def send_notification(data: dict, git_provider: GitProvider):
+    slack_setting = get_settings().slack
     # Send a notification to the slack channel or user
-    slack_channel = get_settings().slack.slack_channel
+    slack_channel = slack_setting.slack_channel
     if slack_channel:
         # Send a message to the slack channel
-        slack_token = get_settings().slack.token
+        slack_token = slack_setting.token
         client = WebClient(token=slack_token)
         mr_details = git_provider.pr
         # Check if is already reviewed with pr agent
@@ -17,24 +19,27 @@ def send_notification(data: dict, git_provider: GitProvider):
                 return
         review_effort = data['review']['estimated_effort_to_review_[1-5]']
         # Grab the number from review effort
-        review_effort = re.findall(r'\d+', review_effort) if review_effort else "N/A"
+        review_effort = re.findall(r'\d+', review_effort) if review_effort else []
         message = """
         @here MR: <{MRLink}|{Title}>\nAuthor: @{Author}\nReview Effort [1-5]: {ReviewEffort}\nFiles changed: {FilesChanged}
         """.format(
             Title=mr_details.title,
-            ReviewEffort=review_effort[0] if review_effort else "N/A",
+            ReviewEffort=review_effort[0] if len(review_effort) > 0 else "N/A",
             Author=mr_details.author['username'],
             FilesChanged=mr_details.changes_count,
             MRLink=git_provider.get_pr_url()
         )
-        client.chat_postMessage(
-            channel=slack_channel, 
-            blocks=[{
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": message
-                }
-            }],
-            text=message
-        )
+        try:
+            client.chat_postMessage(
+                channel=slack_channel, 
+                blocks=[{
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": message
+                    }
+                }],
+                text=message
+            )
+        except Exception as e:
+            get_logger().error(f"Failed to send Slack notification: {e}")
