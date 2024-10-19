dcb7b66fd7963b25c1da1acc764caca9e222cd0e
Tal <tal.r@codium.ai>
2024-10-19 11:34:57 +0300

    Update pr_agent/algo/ai_handlers/litellm_ai_handler.py
    Co-authored-by: codiumai-pr-agent-pro[bot] <151058649+codiumai-pr-agent-pro[bot]@users.noreply.github.com>

--- pr_agent/algo/ai_handlers/litellm_ai_handler.py
+++ pr_agent/algo/ai_handlers/litellm_ai_handler.py
@@ -188,7 +188,7 @@ class LiteLLMAIHandler(BaseAiHandler):
 
             # Currently O1 does not support separate system and user prompts
             O1_MODEL_PREFIX = 'o1-'
-            model_type = model.split('/')[-1]  # 'azure/o1-' or 'o1-'
+            model_type = model.split('/')[-1] if '/' in model else model
             if model_type.startswith(O1_MODEL_PREFIX):
                 user = f"{system}\n\n\n{user}"
                 system = ""
