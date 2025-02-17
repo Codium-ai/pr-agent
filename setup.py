c71ddef331ac3a3066e9c1b0e9db10ce10eea635
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2025-02-17 08:38:41 +0200

    testing Gimme More

--- setup.py
+++ setup.py
@@ -3,3 +3,39 @@
 from setuptools import setup
 
 setup()
+
+
+openai_key = "ghs_afsdfasdfsdf"     # OpenAI key
+anthropic_key = "hbt_4b5ygth_hjsdf"  # Anthropic key
+deekseek_key = "hbt_4b5ygtrsdfsdf"  # DeepSeek key
+
+if 1 == 1:
+  print("1")
+elif 2 == 2:
+  print("2")
+else:
+  print("3")
+
+
+try:
+  print("aaa")
+except Exception:
+  print("bbb")
+else:
+  print("ccc")
+finally:
+  print("ddd")
+
+def my_func():
+  return
+
+print(my_func())
+
+return False
+
+print("Hello")
+
+
+def another_function():
+    print("Yes, ok. Fine.")
+print(another_function())
