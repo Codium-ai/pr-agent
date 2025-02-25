049fc558a8be06f4ab288f3bcd02eb35bd6271cc
Hiroyuki Otomo <42569268+hirobf10@users.noreply.github.com>
2025-02-25 16:51:50 +0900

    Update pr_agent/algo/__init__.py
    Co-authored-by: qodo-merge-pro-for-open-source[bot] <189517486+qodo-merge-pro-for-open-source[bot]@users.noreply.github.com>

--- pr_agent/algo/__init__.py
+++ pr_agent/algo/__init__.py
@@ -59,7 +59,7 @@ MAX_TOKENS = {
     'anthropic/claude-3-opus-20240229': 100000,
     'anthropic/claude-3-5-sonnet-20240620': 100000,
     'anthropic/claude-3-5-sonnet-20241022': 100000,
-    'anthropic/claude-3-7-sonnet-20250219': 100000,
+    'anthropic/claude-3-7-sonnet-20250219': 200000,
     'anthropic/claude-3-5-haiku-20241022': 100000,
     'bedrock/anthropic.claude-instant-v1': 100000,
     'bedrock/anthropic.claude-v2': 100000,
